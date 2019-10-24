13a4bd9554dc24193ea7807e47b30b2e92f8c6bf
audraybinet <audreybinet@yahoo.fr>
2019-10-24 11:52:40 +0200

    exo2

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+var lastName = "John", firstName = "Doe", city = "New York";
+// alert("Prénom:" + lastName + "\n" + "Nom:" firstName + "\n" + "ville:" + city );
+alert(`
+  Nom : ${lastName}
+  Prénom : ${firstName}
+  Ville : ${city}
+  `);
